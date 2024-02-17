3765c319eb0d799985bdcbeae895bf0dc6703e4c
Thunder Cat <thundercatxp@yahoo.com>
2024-02-17 16:21:11 +0300

    update

---
+++ src/lib/translations.js
@@ -0,0 +1,21 @@
+export default {
+    uk: {
+      "homepage.info": "",
+      "card.occupation": "",
+      "card.passion": "",
+      "card.future": "",
+    },
+    pl: {
+      "homepage.info": "",
+      "card.occupation": "",
+      "card.passion": "",
+      "card.future": "",
+    },
+    ru: {
+      "homepage.info": "",
+      "card.occupation": "",
+      "card.passion": "",
+      "card.future": "",
+    }
+  };
+  
