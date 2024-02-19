81a3ae1db7938279c1cc9709df46d2487103f738
Thunder Cat <thundercatxp@yahoo.com>
2024-02-19 22:42:01 +0300

    update

--- src/lib/translation_dropdown.svelte
+++ src/lib/translation_dropdown.svelte
@@ -1,9 +1,8 @@
 <script>
-  import "/node_modules/flag-icons/css/flag-icons.min.css";
   import { t, locale, locales } from "./i18n";
 </script>
 
-<select class="select w-full max-w-xs" bind:value={$locale}>
+<select class="select select-sm w-full max-w-xs" bind:value={$locale}>
   {#each locales as l}
     <option value={l}>{l}</option>
   {/each}
