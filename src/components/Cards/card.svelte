86468c4c2309d915e9e873d60fd507b48eeaf365
ThunderCat <thundercatxp@yahoo.com>
2024-10-21 16:45:43 +0300

    made a single card component

---
+++ src/components/Cards/card.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let src = "";
+  export let title = "";
+  export let text = "";
+</script>
+
+<main>
+  <div class="card w-[24rem] bg-base-200 shadow-xl">
+    <figure class="px-10 pt-10">
+      <img
+        {src}
+        alt=""
+        class="rounded-xl aspect-square object-cover"
+        loading="lazy"
+      />
+    </figure>
+    <div class="card-body items-center text-center">
+      <h2 class="card-title font-bold">{title}</h2>
+      <p>{@html text}</p>
+    </div>
+  </div>
+</main>
+
+<style>
+</style>
--- src/components/Cards/cards.svelte
+++ src/components/Cards/cards.svelte
@@ -1,15 +1,44 @@
 <script>
-  import CardFuture from "./card_future.svelte";
-  import CardOcupation from "./card_ocupation.svelte";
-  import CardPassion from "./card_passion.svelte";
+  //
+
+  import Card from "./card.svelte";
+
+  // ==========================================
+
+  let future = "/future.webp";
+  let passion = "/passion.jpg";
+  let occupation = "/ВЭК.jpg";
+
+  // ==========================================
+
+  // @ts-ignore
+  import { t, locale, locales } from "i18n";
+
+  // Create a locale specific timestamp
+  $: time = new Date().toLocaleDateString($locale, {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 </script>
 
 <main>
   <div class="hero min-h-screen bg-base-100">
     <div class="flex flex-row flex-wrap justify-center gap-7">
-      <div><CardOcupation /></div>
-      <div><CardPassion /></div>
-      <div><CardFuture /></div>
+      <div>
+        <Card
+          src={occupation}
+          title={$t("occupation")}
+          text={$t("card.occupation")}
+        />
+      </div>
+      <div>
+        <Card src={passion} title={$t("passion")} text={$t("card.passion")} />
+      </div>
+      <div>
+        <Card src={future} title={$t("future")} text={$t("card.future")} />
+      </div>
     </div>
   </div>
 </main>
